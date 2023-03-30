f57e6135e1917571a6cbd1384909b05472b3807b
bekishore <bekishore@gmail.com>
2023-03-30 18:11:40 +0800

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
+	name: "kishore.org", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		runs: 1,
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://kishore.org/"
 	]
 };
